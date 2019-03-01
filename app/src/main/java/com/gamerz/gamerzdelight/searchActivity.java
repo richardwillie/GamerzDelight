dd80e59a8e3de6a61c3cf993773a91438bc2c4e5
richard willie <richardwillie.w@gmail.com>
2019-03-01 12:05:48 +0300

    add listView

--- app/src/main/java/com/gamerz/gamerzdelight/searchActivity.java
+++ app/src/main/java/com/gamerz/gamerzdelight/searchActivity.java
@@ -3,6 +3,7 @@ package com.gamerz.gamerzdelight;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -10,7 +11,9 @@ import butterknife.ButterKnife;
 
 public class searchActivity extends AppCompatActivity {
     @BindView(R.id.locationTextView) TextView mLocation;
+    @BindView(R.id.listView)ListView mList;
 
+    private String[] games = new String[] {"resident evil2","mortal kombat xi","Gta v","hitman","tekken6","Need for speed","max payne"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
