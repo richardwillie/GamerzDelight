c228f02e8ef6e5e2d5527ad57432e87fb7d85f63
richard willie <richardwillie.w@gmail.com>
2019-03-01 11:21:40 +0300

    gather user input

--- app/src/main/java/com/gamerz/gamerzdelight/MainActivity.java
+++ app/src/main/java/com/gamerz/gamerzdelight/MainActivity.java
@@ -1,15 +1,34 @@
 package com.gamerz.gamerzdelight;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
-
+    @BindView(R.id.searchButton)Button mSearchButton;
+    @BindView(R.id.locationEditText)EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG,location);
+                Intent intent = new Intent(MainActivity.this,searchActivity.class);
+            }
+        });
     }
 }
