6fcd3c87ede23687242e0c3a4828ff05ba33b868
richard willie <richardwillie.w@gmail.com>
2019-03-01 11:42:05 +0300

    displays searched information

--- app/src/main/java/com/gamerz/gamerzdelight/searchActivity.java
+++ app/src/main/java/com/gamerz/gamerzdelight/searchActivity.java
@@ -1,13 +1,25 @@
 package com.gamerz.gamerzdelight;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class searchActivity extends AppCompatActivity {
+    @BindView(R.id.locationTextView) TextView mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+
+        mLocation.setText("you've searched for these games"+ location);
     }
 }
